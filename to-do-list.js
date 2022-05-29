5252129cc40c83353475ab13720d4b437e91acc2
Anastttasia <nastya.kaneva.99@gmail.com>
2022-05-29 21:01:16 +0300

    add drag&drop items

--- to-do-list.js
+++ to-do-list.js
@@ -2,8 +2,57 @@
 //CONST
 const ITEMS_CONTAINER = document.getElementsByClassName('to-do-items')[0];
 const INPUT_LINE = document.querySelector("input");
+const tasksListElement = document.querySelector(`.to-do-items`);
+const taskElements = tasksListElement.querySelectorAll(`.to-do-items`);
 
-// Создание дела
+
+for (const task of taskElements) {
+    task.draggable = true;
+}
+
+tasksListElement.addEventListener(`dragstart`, (evt) => {
+    evt.target.classList.add(`selected`);
+})
+  
+tasksListElement.addEventListener(`dragend`, (evt) => {
+    evt.target.classList.remove(`selected`);
+});
+
+const getNextElement = (cursorPosition, currentElement) => {
+    const currentElementCoord = currentElement.getBoundingClientRect();
+    const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+    
+    const nextElement = (cursorPosition < currentElementCenter) ?
+      currentElement :
+      currentElement.nextElementSibling;
+    
+    return nextElement;
+  };
+  
+  tasksListElement.addEventListener(`dragover`, (evt) => {
+    evt.preventDefault();
+    
+    const activeElement = tasksListElement.querySelector(`.selected`);
+    const currentElement = evt.target;
+    const isMoveable = activeElement !== currentElement &&
+      currentElement.classList.contains(`item`);
+      
+    if (!isMoveable) {
+      return;
+    }
+    
+    const nextElement = getNextElement(evt.clientY, currentElement);
+    
+    if (
+      nextElement && 
+      activeElement === nextElement.previousElementSibling ||
+      activeElement === nextElement
+    ) {
+      return;
+    }
+          
+      tasksListElement.insertBefore(activeElement, nextElement);
+  });
 
 //Init
 document.querySelector("button").addEventListener("click", () => {
@@ -18,36 +67,35 @@ INPUT_LINE.addEventListener('keydown', (event) =>{
 function addItem(){
     //Create elements
     let divParent = document.createElement('div');
+    let textBlock = document.createElement('div');
     let divChild = document.createElement('div'); 
     let checkbox = document.createElement('input');
-    let trashMark = document.createElement('input');
+    let deleteButton = document.createElement('button');
+
+    //Create class
+    deleteButton.setAttribute('class', 'deleteButton');
+    deleteButton.innerText = 'Удалить';
+    divChild.setAttribute('class', 'flex');
+
 
     divParent.className = "item";
-    divParent.innerHTML = '<div>'+INPUT_LINE.value+'</div>';
-    
-    divChild.style.display = 'flex';
-    divChild.style.alignItems = 'center';
+    divParent.draggable = 'true';
+
+    textBlock.textContent = INPUT_LINE.value;
+    textBlock.setAttribute('class', 'textBlock');
+    divParent.appendChild(textBlock);
+
 
     checkbox.type = 'checkbox';
     checkbox.style.width = '30px';
     checkbox.style.height ='30px';
     divChild.appendChild(checkbox);
 
-    trashMark.type = 'button';
-    trashMark.value = 'Удалить'; 
-    trashMark.style.fontFamily = 'Fredoka One, cursive';
-    trashMark.style.width = '80px';
-    trashMark.style.height ='30px';
-    trashMark.style.borderRadius = '50px'
-    trashMark.style.border = '1px solid red';
-    trashMark.style.marginLeft = '10px';
-    trashMark.style.color = '#FF7D73';
-    divChild.appendChild(trashMark);
 
-    trashMark.addEventListener('click',function(){
+    deleteButton.addEventListener('click',function(){
         divParent.remove();
     })
-    divChild.appendChild(trashMark);
+    divChild.appendChild(deleteButton);
 
     divParent.appendChild(divChild);
     ITEMS_CONTAINER.appendChild(divParent);
